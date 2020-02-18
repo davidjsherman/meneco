ddbf591195c10a4b570791a58e1935bca5b67177
David James Sherman <david.sherman@inria.fr>
2020-02-18 16:25:49 +0100

    Require pytest for testing

--- setup.py
+++ setup.py
@@ -33,5 +33,6 @@ setup(
     package_data={'meneco': ['encodings/*.lp']},
     # scripts          = ['meneco.py'],
     entry_points={'console_scripts': ['meneco = meneco.__main__:main_meneco']},
+    tests_requires=['pytest'],
     install_requires=['pyasp == 1.4.3']
 )
